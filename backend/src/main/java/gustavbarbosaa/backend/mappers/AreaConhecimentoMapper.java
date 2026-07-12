c9d3c08a9dc0c6a1b5acdca476fd6f4d6251eefd
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-07-11 23:22:59 -0300

    feat: cria mapper para novo dto

--- backend/src/main/java/gustavbarbosaa/backend/mappers/AreaConhecimentoMapper.java
+++ backend/src/main/java/gustavbarbosaa/backend/mappers/AreaConhecimentoMapper.java
@@ -3,6 +3,7 @@ package gustavbarbosaa.backend.mappers;
 import gustavbarbosaa.backend.domains.AreaConhecimento;
 import gustavbarbosaa.backend.dtos.requests.AreaConhecimentoRequestDTO;
 import gustavbarbosaa.backend.dtos.responses.AreaConhecimentoResponseDTO;
+import gustavbarbosaa.backend.dtos.responses.MinAreaConhecimentoResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +11,6 @@ public interface AreaConhecimentoMapper {
     AreaConhecimento toEntity(AreaConhecimentoRequestDTO dto);
 
     AreaConhecimentoResponseDTO toDTO(AreaConhecimento entity);
+
+    MinAreaConhecimentoResponseDTO toMinDTO(AreaConhecimento entity);
 }
